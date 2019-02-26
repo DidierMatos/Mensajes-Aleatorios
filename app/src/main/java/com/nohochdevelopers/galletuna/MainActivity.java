062b2453807c54072c00ae2a5a8a8706aa59b48f
Didier Matos <d.matostorres@gmail.com>
2019-02-26 13:31:12 -0500

    Probando log.d y comentando el Toast

--- app/src/main/java/com/nohochdevelopers/galletuna/MainActivity.java
+++ app/src/main/java/com/nohochdevelopers/galletuna/MainActivity.java
@@ -3,6 +3,7 @@ package com.nohochdevelopers.galletuna;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private ConstraintLayout constraintLayout;
 
     @Override
@@ -36,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 txtViewFrase.setText(aleatorio);
                 constraintLayout.setBackgroundColor(color);
 
-                Toast.makeText(MainActivity.this, "Activity created", Toast.LENGTH_SHORT).show();
-
+                //Toast.makeText(MainActivity.this, "Activity created", Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"We're logging from the onCreate() method");
 
             }
         });
